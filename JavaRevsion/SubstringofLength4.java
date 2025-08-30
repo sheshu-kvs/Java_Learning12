2a6e46ccc571a177e70b391bd9afe5b7c86870a2
sheshu-kvs <kvs24161@gmail.com>
2025-08-30 15:50:50 +0530

    SubStringJava

---
+++ JavaRevsion/SubstringofLength4.java
@@ -0,0 +1,14 @@
+public class SubstringofLength4 {
+    public static void main(String[] args) {
+        String str="mississippi";
+        int size=4;
+        for(int i=0;i<str.length()-size;i++){
+            String t="";
+            for(int j=i;j<size+i;j++){
+                t=t+str.charAt(j);
+            }
+            System.out.println(t);
+        }
+    }
+    
+}
