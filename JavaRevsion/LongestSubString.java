21dbd0f0a1d2a8c68826b992ba78859d43d538ba
sheshu-kvs <kvs24161@gmail.com>
2025-08-31 21:00:21 +0530

    LongestSubString

---
+++ JavaRevsion/LongestSubString.java
@@ -0,0 +1,40 @@
+
+import JavaRevsion.
+
+public class LongestSubString {
+    public static void main(String[] args) {
+        String str="mississippi";
+        printSub(str);
+        // System.out.println(s);
+    }
+    private static void  printSub(String s){
+        for(int size=s.length();size>=1;size--){
+            for(int i=0;i<s.length()-size;i++){
+                String t="";
+              for(int j=i;j<i+size;j++){
+                 t=t+s.charAt(j);
+              }
+              if(palin(t)){
+                System.out.println("Longest SubString:"+t);
+                return;
+              }
+            }
+        }
+    }
+
+
+
+
+      public static boolean palin(String s) {
+        String t = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            t = t + s.charAt(i);
+
+        }
+        if (t.equals(s)) {
+            return true;
+        }
+        return false;
+    }
+    
+}
