d457aa47a17ae67678be62ece1ec7f4dc13ad9b8
sheshu-kvs <kvs24161@gmail.com>
2025-08-31 21:01:15 +0530

    WrapperClassExample

---
+++ JavaRevsion/Wrapper.java
@@ -0,0 +1,12 @@
+public class Wrapper {
+    static int a=13;
+    public static void main(String[] args) {
+        Wrapper w1=new Wrapper();
+        // Integer a1=new Integer('a');
+        Integer a1=Integer.valueOf(a);
+        System.out.println("A value::"+a1);
+
+        // System.out.println(w1.a);
+    }
+    
+}
