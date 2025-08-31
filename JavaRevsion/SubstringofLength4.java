d936ba26000e8da170107d93e9b286d86973fb1c
sheshu-kvs <kvs24161@gmail.com>
2025-08-31 21:00:56 +0530

    SubStringWithLength4

--- JavaRevsion/SubstringofLength4.java
+++ JavaRevsion/SubstringofLength4.java
@@ -1,8 +1,10 @@
 public class SubstringofLength4 {
     public static void main(String[] args) {
-        String str="mississippi";
+        String str="Helloworld";
+        // String repla=str.replaceAll("\\s","");
+        // System.out.println(repla);
         int size=4;
-        for(int i=0;i<str.length()-size;i++){
+        for(int i=0;i<=str.length()-size;i++){
             String t="";
             for(int j=i;j<size+i;j++){
                 t=t+str.charAt(j);
@@ -10,5 +12,4 @@ public class SubstringofLength4 {
             System.out.println(t);
         }
     }
-    
 }
