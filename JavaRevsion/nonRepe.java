a973681e801700416be60c9b9b1026e8f7855a71
sheshu-kvs <kvs24161@gmail.com>
2025-09-07 22:09:21 +0530

    Non Repeating First Character

--- JavaRevsion/nonRepe.java
+++ JavaRevsion/nonRepe.java
@@ -1,11 +1,11 @@
 public class nonRepe {
     public static void main(String[] args) {
-        String str="wqmm";
+        String str="swqmm";
         for(int i=0;i<str.length();i++){
             char ch=str.charAt(i);
-            if(!(str.indexOf(ch)==str.lastIndexOf(ch))){
-                System.out.println("Repeating:"+ch);
-                break;
+            if((str.indexOf(ch)==str.lastIndexOf(ch))){
+                System.out.println("Non Repeating: "+ch);
+                return;
             }
         }
     }
