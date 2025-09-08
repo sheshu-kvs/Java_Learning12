963d4d1709eedaabeb883d203c849dcb38e66bbf
sheshu-kvs <kvs24161@gmail.com>
2025-09-08 11:02:13 +0530

    Functional Interface

---
+++ JavaCoding/FunctionalInterface12.java
@@ -0,0 +1,34 @@
+package JavaCoding;
+
+
+// interface greeting{
+//     void sayHello();
+// }
+interface addition{
+    void add(int a,int b);
+}
+
+public class FunctionalInterface12 {
+    public static void main(String[] args) {
+    //   greeting g1=new greeting(){
+    //     public void sayHello(){
+    //         System.out.println("Hey Dude Say Hello");
+    //     }
+    //   };
+
+    //   g1.sayHello();
+    //   to avoid this we are using the lamda
+    // greeting g1=()->
+    // System.out.println("Say Hello!!");
+    //         g1.sayHello();
+
+    // addition a1=new addition(){
+    //    public  void add(int a,int b){
+    //         System.out.println("The Addition of the Two numbers "+(a+b));
+    //     }
+    // };
+    // a1.add(20, 30);
+    // addition a1=(a,b)->a+b;
+    //     System.out.println(a1.add(20, 30));
+    }
+}
