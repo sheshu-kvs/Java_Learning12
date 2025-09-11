aee5cc3b15cfc22d2da6f55a162f05493bccb418
sheshu-kvs <kvs24161@gmail.com>
2025-09-11 17:22:00 +0530

    Pyramid Probelm and Hallow Pyramid

---
+++ JavaRevsion/Pyramid.java
@@ -0,0 +1,52 @@
+package JavaRevsion;
+
+public class Pyramid {
+
+    static void HallowPyramid(int num) {
+        System.out.println(" Pyramid");
+        for (int i = 1; i <= num; i++) {
+            for (int k = 1; k <= num - i + 1; k++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                if (j == 1 || j == i * 2 - 1 || i == num) {
+                    System.out.print(j + " ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println("  ");
+        }
+    }
+
+    static void Fullpyramid(int num) {
+        System.out.println("Pyramid");
+        for (int i = 1; i <= num; i++) {
+            for (int k = 0; k < num - i + 1; k++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println(" ");
+        }
+
+
+
+        static void Rectangle(int num){
+            
+        }
+
+    }
+
+    // Sum of digits until single digit (e.g., 99 → 9+9=18 → 1+8=9).
+    public static void main(String[] args) {
+        int num = 5;
+
+        // Fullpyramid(num);
+        // HallowPyramid(num);
+        Rectangle(num);
+
+    }
+}
