e4cf2f06f6faed086ce70f3f6d5af1b897ca7959
Sheshadri <kvs24161@gmail.com>
2025-11-04 20:35:19 +0530

    Coding Practise on the Recursion

--- Nov/Day4Coding.java
+++ Nov/Day4Coding.java
@@ -9,11 +9,57 @@ public class Day4Coding{
         while(i>=0){
             // System.out.println(str.charAt(i));
              t=t+str.charAt(i);
-            i--;
+             i--;
         }
         System.out.println("Reversed String "+t);
     }
+    static void factNum(int num){
+        int fact=1;
+        while(num>=0){
+            fact = fact * num;
+            num--;
+        }
+        System.out.println("Factorial "+fact);
+    }
+    static int  factNumUsinRecursion(int num){
+        if(num==0){
+            return 1;
+        }
+        return factNumUsinRecursion(num - 1 ) * num; 
+    }
+    static void  fibonaci(int num){
+     int first=0;
+     int second=1;
+    for(int i=0;i<num;i++){
+        System.out.println(first);
+       int next=first+second;
+       int temp = first ;
+      first = second;
+      second = next;  
+    }
+}
+    static int fibonaciRecursion(int num){
+       if(num==0){
+        return 0;
+       }
+       if(num==1){
+        return 1;
+       }
+       return fibonaciRecursion(num-1) + fibonaciRecursion(num-2);
+   
+    }
+    
+    
     public static void main(String[] args) {
-        ReverseString();
+        // ReverseString();
+        // factNum(3);
+        // fibonaci(4);
+     
+        // int num = 100;
+        // for (int i = 0; i < num; i++) {
+        //     System.out.println(fibonaciRecursion(i));
+        // }
+        int res = factNumUsinRecursion(4);
+        System.out.println("Factorial Number "+res);
     }
 }
