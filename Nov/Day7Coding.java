b319727746f977823e59929b17167fe7dccb76ac
Sheshadri <kvs24161@gmail.com>
2025-11-08 08:32:18 +0530

    ReverseDigit,Count Number,Sum of Digit, Armstrong Number

---
+++ Nov/Day7Coding.java
@@ -0,0 +1,57 @@
+public class Day7Coding {
+    static void ReverseNumber(){
+        int num=2345;
+        int rev = 0;
+        while(num>0){
+            int rem = num%10;
+            rev = rev * 10+rem;
+            num=num/10;
+        }
+        System.out.println("Reverse Number "+rev);
+    }
+    static  void sumOfNumber(){
+        int num =2345;
+        int sum = 0;
+        while(num>0){
+            int rem = num%10;
+            sum = sum+rem;
+            num = num /10;
+        }
+        System.out.println("Sum of the Number "+sum);
+    }
+    static  int  CountNumberofDigit(int num){
+        // int num =2345;
+        int count = 0;
+        while(num>0){
+            int rem = num%10;
+            count++;
+            num = num /10;
+        }
+        // System.out.println("Number of the Digits "+count);
+        return count;
+    }
+
+    static void aramstrongNumber(){
+        int num =1513;
+        int original = num;
+        int sum = 0;
+        int numberOfDigit = CountNumberofDigit(num);
+        while(num>0){
+            int rem = num % 10;
+            sum += Math.pow(rem,numberOfDigit);
+            num  = num/10;
+        }
+        if(sum == original){
+            System.out.println("Armstrong Number "+original);
+        }
+        else{
+            System.out.println("Not An Armstrong Number "+original);
+        }
+    }
+    public static void main(String[] args) {
+        // ReverseNumber();
+        // sumOfNumber();
+        // CountNumberofDigit();
+      aramstrongNumber();
+    }
+}
