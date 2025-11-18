d69cb412d0810a0d0802837178d595ca59e76223
Sheshadri <kvs24161@gmail.com>
2025-11-18 10:44:20 +0530

    Day18Coding

--- Nov/Day17Coding.java
+++ Nov/Day17Coding.java
@@ -1,56 +1,68 @@
+
 public class Day17Coding {
-    static void UpperCaseFirstLettar(){
-        String str="java,morin,selva";
-        String stringWords[]=str.split(",");
+
+    static void UpperCaseFirstLettar() {
+        String str = "java,morin,selva";
+        String stringWords[] = str.split(",");
         StringBuilder sb = new StringBuilder();
-        for(String word:stringWords){
-            String t="";
-            for(int i=0;i<word.length();i++){
+        for (String word : stringWords) {
+            String t = "";
+            for (int i = 0; i < word.length(); i++) {
                 char ch = word.charAt(i);
-               if(i==0){
-                 sb.append(Character.toUpperCase(ch));
-               }    
-               else{
-                sb.append(ch);
-               }
-               
+                if (i == 0) {
+                    sb.append(Character.toUpperCase(ch));
+                } else {
+                    sb.append(ch);
+                }
+
             }
-            if(!word.equals(stringWords[stringWords.length-1]));{
+            if (!word.equals(stringWords[stringWords.length - 1]));
+            {
                 sb.append(" ");
-            } 
-                                                                                                                                                                                                                                                                                                                      
+            }
+
         }
-         System.out.println(sb);  
+        System.out.println(sb);
     }
 
-
-    static void WithoutSb(){
-        String str="rahul,rain,partiban";
+    static void WithoutSb() {
+        String str = "rahul,rain,partiban";
         String stringWords[] = str.split(",");
-        String result="";
-        for(String word:stringWords){
-            String currentWord="";
-            for(int i=0;i<word.length();i++){
-                char ch=word.charAt(i);
-                if(i==0){
-                    currentWord= currentWord+Character.toUpperCase(ch);
-                }
-                else{
-                    currentWord = currentWord +ch;
+        String result = "";
+        for (String word : stringWords) {
+            String currentWord = "";
+            for (int i = 0; i < word.length(); i++) {
+                char ch = word.charAt(i);
+                if (i == 0) {
+                    currentWord = currentWord + Character.toUpperCase(ch);
+
+                } else {
+                    currentWord = currentWord + ch;
                 }
             }
-            result = result+currentWord;
+            result = result + currentWord;
 
-            if(!word.equals(stringWords[stringWords.length-1])){
+            if (!word.equals(stringWords[stringWords.length - 1])) {
                 result = result + " ";
             }
-           
+
         }
-         System.out.println(result);
+        System.out.println(result);
     }
+
+    static void firstLettarAndLast() {
+        String str = "word";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(0);
+            int temp = ch;
+
+        }
+        System.out.println(str.length() - 1);
+    }
+
     public static void main(String[] args) {
         // UpperCaseFirstLettar();
-        WithoutSb();
+        // WithoutSb();
+        firstLettarAndLast();
     }
-    
 }
