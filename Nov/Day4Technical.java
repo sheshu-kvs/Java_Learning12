bfc69bd4df1ea8b63418b007e29239488632f6a2
Sheshadri <kvs24161@gmail.com>
2025-11-24 22:37:13 +0530

    Modified

--- Nov/Day4Technical.java
+++ Nov/Day4Technical.java
@@ -1,15 +1,14 @@
-package Nov;
 
-public class Day4Technical {
+// public class Day4Technical {
     
-    public static void main(String[] args) {
-        String str="Hello";
-        for(int i=0;i<str.length();i++){
-            char ch = str.charAt(i);
-            // System.out.println(str.indexOf(ch));
-            System.out.println(str.lastIndexOf(ch));
-        }
+//     public static void main(String[] args) {
+//         String str="Hello";
+//         for(int i=0;i<str.length();i++){
+//             char ch = str.charAt(i);
+//             // System.out.println(str.indexOf(ch));
+//             System.out.println(str.lastIndexOf(ch));
+//         }
         
-    }
+//     }
     
-}
+// }
--- Nov/Day5Technical.java
+++ Nov/Day5Technical.java
@@ -1,58 +1,58 @@
-package Nov;
-/*Encapsulation
- * an Encapslaton is the one of h important concep of the oops
- * Each And Every Object Was Very Important part that is the data to provide the data to the security..
- * controlled access through the setters and the getters..
- * Example:
- * An Bank Class with the balance property.. the withdrawal amount is always <balance then we are getting the balance..
- * 
- *  
- * 
- * Inhertency 
- * An inhertency is the accquring the properties and the methods  from the parent class to the child class  
- * 
- * Polymorhism 
- * An polymorhism mean parent type refernce and he child objects is called as the  polymorphism....
- */
 
-    class school{
-        private int marks;
+// /*Encapsulation
+//  * an Encapslaton is the one of h important concep of the oops
+//  * Each And Every Object Was Very Important part that is the data to provide the data to the security..
+//  * controlled access through the setters and the getters..
+//  * Example:
+//  * An Bank Class with the balance property.. the withdrawal amount is always <balance then we are getting the balance..
+//  * 
+//  *  
+//  * 
+//  * Inhertency 
+//  * An inhertency is the accquring the properties and the methods  from the parent class to the child class  
+//  * 
+//  * Polymorhism 
+//  * An polymorhism mean parent type refernce and he child objects is called as the  polymorphism....
+//  */
 
-        public void setmarks(int marks){
-            if(marks<=100){
-                this.marks = marks;
-                System.out.println("You are Passeed!!!");
-            }
-            else{
-                System.out.println("please Enter the Valid Marks");
-            }
-        }
+//     class school{
+//         private int marks;
+
+//         public void setmarks(int marks){
+//             if(marks<=100){
+//                 this.marks = marks;
+//                 System.out.println("You are Passeed!!!");
+//             }
+//             else{
+//                 System.out.println("please Enter the Valid Marks");
+//             }
+//         }
 
 
-        // public school(int marks){
-        //    if(marks<=100){
-        //     //here we are setting the marks private variable...
-        //     this.marks = marks;
-        //         System.out.println("You are Passeed!!!");
-        //     }
-        //     else{
-        //         System.out.println("please Enter the Valid Marks");
-        //     }
-        // }
-        public int getmarks(){
-            return marks;
-        }
+//         // public school(int marks){
+//         //    if(marks<=100){
+//         //     //here we are setting the marks private variable...
+//         //     this.marks = marks;
+//         //         System.out.println("You are Passeed!!!");
+//         //     }
+//         //     else{
+//         //         System.out.println("please Enter the Valid Marks");
+//         //     }
+//         // }
+//         public int getmarks(){
+//             return marks;
+//         }
         
         
-    }
+//     }
 
-public class Day5Technical {
-    public static void main(String[] args) {
-        school s1=new school();
-    //    school s1 = new school(99);
-        s1.setmarks(109);
-       int res =  s1.getmarks();
-       System.out.println("marks = "+res);
-    }
+// public class Day5Technical {
+//     public static void main(String[] args) {
+//         school s1=new school();
+//     //    school s1 = new school(99);
+//         s1.setmarks(109);
+//        int res =  s1.getmarks();
+//        System.out.println("marks = "+res);
+//     }
     
-}
+// }
